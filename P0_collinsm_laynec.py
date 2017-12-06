4e8d50de4582706a3dd53ca250d089a41f846f10
maddiebwcollins <collinsm@berea.edu>
2017-12-06 18:17:27 -0500

    DONE (I hope)

--- P0_collinsm_laynec.py
+++ P0_collinsm_laynec.py
@@ -5,7 +5,7 @@
 # Assignment: P0
 #
 ######################################################################
-import random
+import random     #import modules
 import turtle
 
 class Shapes:
@@ -17,65 +17,58 @@ class Shapes:
         :param prim: This was originally 's' in the previous code. It is the primary number in out algorithm. It is user input.
         :param secon: Also used to be a part of 's'. It is the secondary number to make the shapes. Also user input.
         :param col: Sets the color of the shape. User can pick a color or it can be random.
-        :param coord: Sets the coordinates of the shape.
         """
 
         self.prim = prim
         self.secon = secon
         self.col = col
-        # self.coord = coord
 
-    def draw_random(self, prim, secon, col):
+    def draw_random(self, prim, secon, col):  #This function is what draws the shapes.
         turt = turtle.Turtle()
         wn = turtle.Screen()
         wn.setup (width=1400, height=700, startx=0, starty=0)
+        wn.onclick(turt.goto)      #This lets the user interact live by moving the turtle to the clicked location.
 
-        """
-        Draws a randomly colored shape using the turtle library
-
-        :param turt: a turtle object to draw with
-        :return: None
-        """
-        if self.col == str("random"):
+        if self.col == str("random"):      #If user picks random, this allows for a random RGB color value to be selected.
             wn.colormode(255)
             self.col = ((random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)))    # gives the turtle a random color
             turt.pencolor((self.col))
-        else:
+        else:                            #Otherwise, the color is what the user selects.
             turt.pencolor(self.col)
 
-        while True:
+        while True:                     #This allows the code to repeat indefinatly.
 
             for num in range(10):
-                if prim == "random":
+                if prim == "random":             #If the user types random, a random integer is selected for the primary number.
                     p = random.randint(3,10)
-                else:
+                else:                            #Otherwise, their choice is used.
                     p = int(prim)
-                if secon == "random":
-                    s = random.randint(3,10)
-                else:
+                if secon == "random":             #If the user types random, a random integer is selected for the secondary number.
+                    s = random.randint(1,10)
+                else:                            #Otherwise, their choice is used.
                     s = int(secon)
                 turt.penup()
-            # Move the turtle to a random place on the screen
-                turt.goto(random.randint(-650, 650), random.randint(-300, 300))
+                turt.goto(random.randint(-650, 650), random.randint(-300, 300))    # Move the turtle to a random place on the screen
                 turt.pendown()
                 for num in range(p):
                     turt.forward(50)
-                    turt.left(360/p)           # the angle ensures a perfect hexagon
+                    turt.left(360/p)
 
-                    for num in range (s//2):
-                        turt.forward(25)
+                    for num in range (s//2):       #This is the magical kaleidoscope algorithim that makes the cool shapes;
+                        turt.forward(25)           #honestly, we're not sure how it works. But it sure looks cool.
                         turt.left(360//p)
         wn.exitonclick()
+
 
 
 def main():
 
     prim = (input("Pick a number between 3 and 10, or type 'random'. "))
-    secon = (input("Pick a second number between 3 and 10, or type 'random'. "))
+    secon = (input("Pick a second number between 3 and 10, or type 'random'. "))          #Inputs to select the values.
     col = str(input("Pick a color, or type 'random'. "))
     Shape = Shapes(prim, secon, col)
 
-    Shape.draw_random(prim, secon, col)
+    Shape.draw_random(prim, secon, col)                    #Call the function.
 
 if __name__ == "__main__":
     main()
